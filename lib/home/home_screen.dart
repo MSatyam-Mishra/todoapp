0106fa7449eecd9d3c6c95cb8a0b5d724d0afd1d
MSatyam-Mishra <satyam12295@gmail.com>
2023-11-04 16:35:02 +0530

    commit

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,30 +1,10 @@
-// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class HomePageSc extends StatelessWidget {
+  const HomePageSc({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [],
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
